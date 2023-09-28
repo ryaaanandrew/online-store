7e8f7e173cdfaaf473cf6d0405c3a36dd046aae4
Ryan Andrew <ryanandrew.sibat@gmail.com>
2023-09-28 17:18:46 -0400

    fixed body height issue

--- app/(collections)/collections/[collectionName]/products/[productId]/page.tsx
+++ app/(collections)/collections/[collectionName]/products/[productId]/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const CollectionProductPage = () => {
-  return <div>CollectionProductPage</div>;
+  return (
+    <div className="flex border-2 border-red-400 h-full">
+      <div className="border-2 w-full">
+        <h1>left</h1>
+      </div>
+      <div className="border-2 border-blue-500 w-full">right</div>
+    </div>
+  );
 };
 
 export default CollectionProductPage;
