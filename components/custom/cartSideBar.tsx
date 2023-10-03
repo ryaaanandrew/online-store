28da409b11bcfb2fea09be0ce0ebed674ba21714
Ryan Andrew <ryanandrew.sibat@gmail.com>
2023-10-03 16:52:19 -0400

    added cart display

--- components/custom/cartSideBar.tsx
+++ components/custom/cartSideBar.tsx
@@ -9,24 +9,49 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { useSidebar } from "@/hooks/use-sidebar";
+import { Separator } from "../ui/separator";
+import { useEffect } from "react";
+import { ProductType, useCart } from "@/hooks/use-cart";
+import { formatCurrency } from "@/lib/formatCurrency";
+import Image from "next/image";
 
 const CartSidebar = () => {
   const { isSidebarOpen, toggleSidebar } = useSidebar();
+  const { cart } = useCart();
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
 
   return (
     <Sheet open={isSidebarOpen} onOpenChange={() => toggleSidebar(false)}>
-      <SheetTrigger>Open</SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+          <SheetTitle>Shopping Cart</SheetTitle>
+          <Separator />
+          {cart?.map((product: ProductType) => (
+            <CartItem product={product} key={product.id} />
+          ))}
         </SheetHeader>
       </SheetContent>
     </Sheet>
   );
 };
+
+const CartItem = ({ product }: { product: ProductType }) => (
+  <div className="flex justify-start items-center">
+    <Image
+      height={70}
+      width={70}
+      src={product?.previewImgUrls?.[0]}
+      alt={`image of ${product.name}`}
+      className="mr-4"
+    />
+    <div>
+      <h2>Product: {product.name}</h2>
+      <h2>Price: {formatCurrency(product.price)}</h2>
+    </div>
+  </div>
+);
 
 export default CartSidebar;
