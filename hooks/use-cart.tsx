96e0ceb0639e57910ac4cdde44d67d0150738c1c
Ryan Andrew <ryanandrew.sibat@gmail.com>
2023-10-04 15:09:56 -0400

    added abilit to persist date via zustand

--- hooks/use-cart.tsx
+++ hooks/use-cart.tsx
@@ -1,4 +1,6 @@
+import { get } from "http";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export type ProductType = {
   id: string;
@@ -21,31 +23,38 @@ interface CartStore {
   removeFromCart: (product: ProductTypeInCart) => void;
 }
 
-export const useCart = create<CartStore>((set, get) => ({
-  cart: [],
-  addToCart: (product: ProductTypeInCart) => {
-    const currentCart = get().cart;
-    const productSizeId = `${product.id}-${product.size}`;
-    const updatedProduct = { ...product, productSizeId };
-    const prodIdx = currentCart.findIndex(
-      (item) => item.productSizeId === productSizeId
-    );
+export const useCart = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      addToCart: (product: ProductTypeInCart) => {
+        const currentCart = get().cart;
+        const productSizeId = `${product.id}-${product.size}`;
+        const updatedProduct = { ...product, productSizeId };
+        const prodIdx = currentCart.findIndex(
+          (item) => item.productSizeId === productSizeId
+        );
 
-    if (prodIdx !== -1) {
-      currentCart[prodIdx].quantity =
-        currentCart[prodIdx].quantity + product.quantity;
+        if (prodIdx !== -1) {
+          currentCart[prodIdx].quantity =
+            currentCart[prodIdx].quantity + product.quantity;
 
-      set(() => ({ cart: [...currentCart] }));
-    } else {
-      set((state) => ({ cart: [...state.cart, updatedProduct] }));
+          set(() => ({ cart: [...currentCart] }));
+        } else {
+          set((state) => ({ cart: [...state.cart, updatedProduct] }));
+        }
+      },
+      removeFromCart: (product: ProductTypeInCart) => {
+        const currentCart = get().cart;
+        const fitleredCart = currentCart.filter((item) => {
+          return item.productSizeId !== product.productSizeId;
+        });
+
+        set(() => ({ cart: [...fitleredCart] }));
+      },
+    }),
+    {
+      name: "online-store-storage",
     }
-  },
-  removeFromCart: (product: ProductTypeInCart) => {
-    const currentCart = get().cart;
-    const fitleredCart = currentCart.filter((item) => {
-      return item.productSizeId !== product.productSizeId;
-    });
-
-    set(() => ({ cart: [...fitleredCart] }));
-  },
-}));
+  )
+);
